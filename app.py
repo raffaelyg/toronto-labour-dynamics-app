1893b1b132be468097507eb418cdc6a0e115a908
raffaelyg <raffael.yg@gmail.com>
2026-02-24 10:44:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,46 @@ st.set_page_config(page_title="GTA Strategy Explorer", layout="wide")
 
 # --- DATA INGESTION LOGIC (Self-Healing) ---
 @st.cache_data
+
+import requests
+ 
+# Toronto Open Data is stored in a CKAN instance. It's APIs are documented here:
+# https://docs.ckan.org/en/latest/api/
+ 
+# To hit our API, you'll be making requests to:
+base_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca"
+ 
+# Datasets are called "packages". Each package can contain many "resources"
+# To retrieve the metadata for this package and its resources, use the package name in this page's URL:
+url = base_url + "/api/3/action/package_show"
+params = { "id": "municipal-licensing-and-standards-business-licences-and-permits"}
+package = requests.get(url, params = params).json()
+ 
+# To get resource data:
+for idx, resource in enumerate(package["result"]["resources"]):
+ 
+       # for datastore_active resources:
+       if resource["datastore_active"]:
+ 
+           # To get all records in CSV format:
+           url = base_url + "/datastore/dump/" + resource["id"]
+           resource_dump_data = requests.get(url).text
+           print(resource_dump_data)
+ 
+           # To selectively pull records and attribute-level metadata:
+           url = base_url + "/api/3/action/datastore_search"
+           p = { "id": resource["id"] }
+           resource_search_data = requests.get(url, params = p).json()["result"]
+           print(resource_search_data)
+           # This API call has many parameters. They're documented here:
+           # https://docs.ckan.org/en/latest/maintaining/datastore.html
+ 
+       # To get metadata for non datastore_active resources:
+       if not resource["datastore_active"]:
+           url = base_url + "/api/3/action/resource_show?id=" + resource["id"]
+           resource_metadata = requests.get(url).json()
+           print(resource_metadata)
+           # From here, you can use the "url" attribute to download this file
 def load_data():
     """
     Checks for local data. If missing, fetches directly from Toronto Open Data API.
@@ -23,7 +63,7 @@ def load_data():
         # If file is missing on GitHub/Streamlit Cloud, fetch it live
         with st.spinner("Fetching latest data from Toronto Open Data Portal..."):
             # Direct link to the 'Business Licensing' CSV resource
-            csv_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/business-licensing-all-categories/resource/8834458b-a7e1-4328-8686-22a84a282f9d/download/business-licences-all-categories.csv"
+            csv_url = url
             headers = {"User-Agent": "Mozilla/5.0"}
             
             try:
